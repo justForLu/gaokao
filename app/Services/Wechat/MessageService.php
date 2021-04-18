cf7121681e6e0b971e197e7a7eef5e9c60183286
justForLu <1120714124@qq.com>
2021-04-18 23:12:07 +0800

    PC文件

--- app/Services/Wechat/MessageService.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Services\Wechat;
-
-class MessageService
-{
-}
--- config/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'page_size' => 10,
-    'uploadImg' => '/upload/admin/image',
-    'document_root' => $_SERVER['DOCUMENT_ROOT']
-];
