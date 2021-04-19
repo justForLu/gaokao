f29a9bfc91460fe957caaacda547ae763ebf1a22
justForLu <1120714124@qq.com>
2021-04-19 10:07:10 +0800

    H5

--- resources/views/admin/public/css.blade.php
+++
@@ -1,4 +0,0 @@
-<link rel="shortcut icon" href="{{asset("/assets/favicon.ico")}}" type="image/x-icon">
-<link rel="stylesheet" href="{{asset("/assets/plugins/layuiadmin/layui/css/layui.css")}}">
-<link rel="stylesheet" href="{{asset("/assets/plugins/layuiadmin/style/admin.css")}}">
-<link rel="stylesheet" href="{{asset("/assets/plugins/layuiadmin/lib/font-awesome-4.7.0/css/font-awesome.min.css")}}">
